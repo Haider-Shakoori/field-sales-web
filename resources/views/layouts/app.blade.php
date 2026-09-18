cba74d16279f3389af841f3a74ce03d6674c6307
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-18 22:20:59 +0430

    feat(batch3): expose both assignment histories

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,8 @@
                     <a href="{{ route('admin.salesmen.index') }}" class="rounded-lg px-3 py-2 hover:bg-white/10">Salesmen</a>
                     <a href="{{ route('admin.supervisors.index') }}" class="rounded-lg px-3 py-2 hover:bg-white/10">Supervisors</a>
                     <a href="{{ route('admin.devices.index') }}" class="rounded-lg px-3 py-2 hover:bg-white/10">Devices</a>
-                    <a href="{{ route('admin.salesman-assignments.index') }}" class="rounded-lg px-3 py-2 hover:bg-white/10">Assignments</a>
+                    <a href="{{ route('admin.salesman-assignments.index') }}" class="rounded-lg px-3 py-2 hover:bg-white/10">Salesman assignments</a>
+                    <a href="{{ route('admin.supervisor-assignments.index') }}" class="rounded-lg px-3 py-2 hover:bg-white/10">Supervisor assignments</a>
                 @endif
                 @if($currentUser->hasPermission('audit:view'))
                     <a href="{{ route('admin.audit.index') }}" class="rounded-lg px-3 py-2 hover:bg-white/10">Audit</a>
