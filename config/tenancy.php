a9bb00bdeb79d5cabffb5ea72606bc8bae9f3ce8
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-18 19:58:27 +0430

    feat: add infrastructure configuration

---
+++ config/tenancy.php
@@ -0,0 +1,2 @@
+<?php
+return ['tracking'=>['max_batch_points'=>(int)env('FIELD_SALES_MAX_BATCH_POINTS',100)],'defaults'=>['work_session_start_mode'=>'manual','workday_start_time'=>'08:00','workday_end_time'=>'17:00','auto_end_session'=>false,'gps_tracking_enabled'=>true,'gps_moving_interval_seconds'=>15,'gps_stationary_interval_seconds'=>60,'gps_stale_after_minutes'=>15,'privacy_policy_version'=>'1']];
