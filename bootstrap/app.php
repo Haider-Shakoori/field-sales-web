c693967d01bee9235e439ba7d001f19ffbd9973d
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-18 20:51:24 +0430

    feat: register permission middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(web: __DIR__.'/../routes/web.php', api: __DIR__.'/../routes/api.php', commands: __DIR__.'/../routes/console.php', health: '/up')
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->alias(['device.required' => DeviceRequired::class]);
+        $middleware->alias([\n            'device.required' => DeviceRequired::class,\n            'permission' => \\App\\Http\\Middleware\\PermissionRequired::class,\n        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(fn (Request $request) => $request->is('api/*'));
