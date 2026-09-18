4f20089ff316dca9aaad3fca96432911b45cf413
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-18 19:57:33 +0430

    feat: add web configuration and services

---
+++ app/View/Components/Layouts/App.php
@@ -0,0 +1,3 @@
+<?php
+namespace App\View\Components\Layouts; use Illuminate\View\Component; use Illuminate\View\View;
+class App extends Component { public function render():View{return view('layouts.app');} }
