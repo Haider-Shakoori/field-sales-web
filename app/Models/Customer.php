beccbee1ba9f62f8babdd39b0aa6122358fe5195
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-19 08:09:10 +0430

    Batch 8: relate customers to call activities

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -62,6 +62,11 @@ class Customer extends Model
         return $this->hasMany(CustomerVisit::class);
     }
 
+    public function callActivities(): HasMany
+    {
+        return $this->hasMany(CustomerCallActivity::class)->latest('called_at');
+    }
+
     public function scopeActive(Builder $query): Builder
     {
         return $query->where('is_active', true);
