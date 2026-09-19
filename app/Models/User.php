3632b5692ef66cf2cad9629941913bdd29993777
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-19 12:52:07 +0430

    Batch 14: add user notification relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use App\Tenancy\TenantContextMismatchException;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -42,6 +43,16 @@ class User extends Authenticatable
     public function supervisor(): HasOne
     {
         return $this->hasOne(Supervisor::class);
+    }
+
+    public function operationalNotifications(): HasMany
+    {
+        return $this->hasMany(OperationalNotification::class);
+    }
+
+    public function notificationPreference(): HasOne
+    {
+        return $this->hasOne(NotificationPreference::class);
     }
 
     public function roles(): BelongsToMany
