13c8aa9b549b138a7f17a8dcd9fc238c95c0cf95
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-25 02:08:45 +0430

    Add leads pipeline support

--- app/Models/Salesman.php
+++ app/Models/Salesman.php
@@ -37,6 +37,16 @@ class Salesman extends Model
         return $this->hasMany(SalesmanAssignment::class);
     }
 
+    public function leads(): HasMany
+    {
+        return $this->hasMany(Lead::class, 'assigned_salesman_id');
+    }
+
+    public function leads(): HasMany
+    {
+        return $this->hasMany(Lead::class, 'assigned_salesman_id');
+    }
+
     public function collections(): HasMany
     {
         return $this->hasMany(Collection::class)->latest('collected_at');
