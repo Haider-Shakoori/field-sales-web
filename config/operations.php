5c61bdf66d1ab89f8e73322ab7c88cf4022d48cb
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-25 10:49:11 +0430

    Add shared-hosting operations settings

--- config/operations.php
+++ config/operations.php
@@ -7,8 +7,14 @@ return [
     ],
 
     'monitoring' => [
+        'require_scheduler_heartbeat' => (bool) env('FIELD_SALES_REQUIRE_SCHEDULER_HEARTBEAT', false),
+        'scheduler_heartbeat_max_age_seconds' => (int) env('FIELD_SALES_SCHEDULER_HEARTBEAT_MAX_AGE_SECONDS', 180),
         'max_failed_jobs' => (int) env('FIELD_SALES_MONITOR_MAX_FAILED_JOBS', 10),
         'max_queued_jobs' => (int) env('FIELD_SALES_MONITOR_MAX_QUEUED_JOBS', 1000),
         'max_oldest_job_seconds' => (int) env('FIELD_SALES_MONITOR_MAX_OLDEST_JOB_SECONDS', 600),
+    ],
+
+    'shared_hosting' => [
+        'scheduled_backups' => (bool) env('FIELD_SALES_SCHEDULE_BACKUPS', false),
     ],
 ];
